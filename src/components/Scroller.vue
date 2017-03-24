8cf5893ba86dd845a9bc65b3063de5ffebfa73ba
NelsonKuang <410374308@qq.com>
2017-03-24 11:00:07 +0800

    delete scope for css

--- src/components/Scroller.vue
+++ src/components/Scroller.vue
@@ -284,8 +284,7 @@ export default {
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
 .vscroller{
   font-family: arial,verdana,sans-serif;
   padding: .5em .25em;
